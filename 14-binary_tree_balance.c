47c640c515dff81b8c9d544ba50a04cce05db745
jeff <jeffrey-31@hotmail.fr>
2025-04-07 21:49:53 +0200

    measures the balance factor of a binary tree

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,19 +8,18 @@
  * Return: balance factor
  */
 
-size_t binary_tree_balance(const binary_tree_t *tree)
+int binary_tree_balance(const binary_tree_t *tree)
 {
 	int left, right;
 
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left == NULL && tree->right == NULL)
-		return (-1);
-
-	left = binary_tree_nodes(tree->left) + 1;
-
-	right = binary_tree_nodes(tree->right) + 1;
-
+	left = binary_tree_height(tree->left);
+	if (tree->left == NULL)
+		left = -1;
+	right = binary_tree_height(tree->right);
+	if (tree->right == NULL)
+		right = -1;
 	return (left - right);
 }
